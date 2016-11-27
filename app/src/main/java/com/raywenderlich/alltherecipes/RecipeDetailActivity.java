6db3a3e48337daeaecb257171815b7057ec2a6a1
shanhong cheng <shnahongcheng@gmail.com>
2016-11-26 17:54:00 -0700

    initial commit

---
+++ app/src/main/java/com/raywenderlich/alltherecipes/RecipeDetailActivity.java
@@ -0,0 +1,28 @@
+package com.raywenderlich.alltherecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+
+        String title = this.getIntent().getExtras().getString("title");
+        String url = this.getIntent().getExtras().getString("url");
+
+// 2
+        setTitle(title);
+
+// 3
+        mWebView = (WebView) findViewById(R.id.detail_web_view);
+
+// 4
+        mWebView.loadUrl(url);
+    }
+}
